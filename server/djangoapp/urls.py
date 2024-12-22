1db4cf3e92eeabaeed3ccc9bc34c2ca3e7a2443d
vamegah <makvim@yahoo.com>
2024-12-22 18:19:00 -0500

    Added car model and make

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route="logout", view=views.logout_request, name="logout"),
     path('register/', views.registration, name='register'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view
 
