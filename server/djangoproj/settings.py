bf0a908b6be8d1c32fb0828dd4d7a73bc27f9b45
vamegah <102638632+vamegah@users.noreply.github.com>
2024-12-23 15:10:47 -0600

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,10 +31,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://vamega13-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    'https://vamega13-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
+    'proxy.cognitiveclass.ai',
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://vamega13-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    'https://vamega13-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
+    'proxy.cognitiveclass.ai',
 ]
 
 REST_FRAMEWORK = {
@@ -100,7 +102,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
